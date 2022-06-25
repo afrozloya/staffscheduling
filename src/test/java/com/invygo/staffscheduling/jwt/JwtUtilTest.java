6b6398f8861467a3c07516055894a3aa51fcb1db
afrozloya <aloya.effcon@gmail.com>
2022-06-26 00:37:59 +0400

    added jacoco report

---
+++ src/test/java/com/invygo/staffscheduling/jwt/JwtUtilTest.java
@@ -0,0 +1,16 @@
+package com.invygo.staffscheduling.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(MockitoJUnitRunner.class)
+class JwtUtilTest {
+
+    @Test
+    void createToken() {
+        assertEquals(1,1);
+    }
+}
